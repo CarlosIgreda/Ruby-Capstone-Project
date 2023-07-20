fc094fe374a0cbea5a48ee5283a153500c7ba53e
CarlosIgreda <cjigredarivas@gmail.com>
2023-07-20 09:40:30 -0500

    Update book.rb

--- book.rb
+++ book.rb
@@ -4,7 +4,7 @@ require_relative 'item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize_additional_info(publisher, cover_state)
+  def initialize(publisher, cover_state)
     @publisher = publisher
     @cover_state = cover_state
   end
