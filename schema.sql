2fed7eb371543dbb2f703d7fb78b317ba45893fd
CarlosIgreda <cjigredarivas@gmail.com>
2023-07-21 03:23:04 -0500

    Update schema file

--- schema.sql
+++ schema.sql
@@ -2,13 +2,18 @@ DROP TABLE IF EXISTS Item;
 DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Author;
 DROP TABLE IF EXISTS MusicAlbum;
+DROP TABLE IF EXISTS Labels;
+DROP TABLE IF EXISTS Books;
+DROP TABLE IF EXISTS Games;
 
 CREATE TABLE Item (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  archived BOOLEAN NOT NULL,
-  PRIMARY KEY (id)
+  id INTEGER PRIMARY KEY,
+  genre_id INT REFERENCES genres(id),
+  author_id INT REFERENCES authors(id),
+  label_id INT REFERENCES labels(id),
+  publish_date DATE,
+  archived BOOLEAN
 );
-
 
 CREATE TABLE Genre (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -29,3 +34,22 @@ CREATE TABLE MusicAlbum (
   FOREIGN KEY (id) REFERENCES Item(id),
   FOREIGN KEY (genre_id) REFERENCES Genre(id)
 );
+
+CREATE TABLE Labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  color VARCHAR(255)
+);
+
+CREATE TABLE Books (
+  id INT PRIMARY KEY REFERENCES items(id),
+  title VARCHAR(255),
+  publisher VARCHAR(255),
+  cover_state VARCHAR(255)
+);
+
+CREATE TABLE Game(
+  id INT PRIMARY KEY REFERENCES items(id),
+  last_played_at DATE NOT NULL,
+  multiplayer NULL
+)
